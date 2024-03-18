f8cd814904baf67b3a3cd6eae8acce883b0a8399
[Mohammad Rizwan] <[mrsarwar@alu.ucam.edu]>
2024-03-18 10:26:02 +0100

    Agregada documentación JavaDoc a todas las clases, atributos y métodos.

--- Empleado.java
+++ Empleado.java
@@ -1,14 +1,52 @@
+/**
+ * Clase que representa a un empleado en el sistema.
+ */
 class Empleado {
-    String nombre;
-    String cargo;
-    double salario;
+    private String nombre;
+    private String cargo;
+    private double salario;
 
+     /**
+     * Constructor de la clase Empleado.
+     * @param nombre El nombre del empleado.
+     * @param cargo El cargo del empleado.
+     * @param salario El salario del empleado.
+     */
     public Empleado(String nombre, String cargo, double salario) {
         this.nombre = nombre;
         this.cargo = cargo;
         this.salario = salario;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+
+    /**
+     * Devuelve una representación en formato de cadena de caracteres del objeto Empleado.
+     * @return La representación en formato de cadena de caracteres del objeto Empleado.
+     */
     @Override
     public String toString() {
         return "Empleado{" +
--- Empleados.java
+++ Empleados.java
@@ -1,17 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que gestiona a los empleados en el sistema.
+ */
 public class Empleados {
     private List<Empleado> lista;
 
+    /**
+     * Constructor de la clase Empleados.
+     */
     public Empleados() {
         this.lista = new ArrayList<>();
     }
 
+    /**
+     * Añade un nuevo empleado a la lista de empleados.
+     * @param nombre El nombre del nuevo empleado.
+     * @param cargo El cargo del nuevo empleado.
+     * @param salario El salario del nuevo empleado.
+     */
     public void altaEmpleado(String nombre, String cargo, double salario) {
         lista.add(new Empleado(nombre, cargo, salario));
     }
 
+
+    /**
+     * Aumenta el salario de todos los empleados en un porcentaje dado.
+     * @param porcentaje El porcentaje de aumento de salario.
+     */
     public void aumentarSalario(double porcentaje) {
         for (Empleado empleado : lista) {
             double nuevoSalario = empleado.getSalario() * (1 + porcentaje / 100);
@@ -19,6 +36,10 @@ public class Empleados {
         }
     }
 
+
+    /**
+     * Muestra la lista de empleados en el sistema.
+     */
     public void mostrarEmpleados() {
         for (Empleado empleado : lista) {
             System.out.println(empleado);
