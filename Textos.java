2618c6f442fd80f0e329e6a361a1a583c4f538a8
[Mohammad Rizwan] <[mrsarwar@alu.ucam.edu]>
2024-03-18 10:55:58 +0100

    Modificacion del README, esto lleva una imagen del editor de codigo

--- Textos.java
+++ Textos.java
@@ -1,4 +1,11 @@
 public class Textos {
+    /**
+     * Mensaje utilizado para solicitar al usuario que introduzca el porcentaje de aumento de salario.
+     */
     public static final String MENSAJE_PORCENTAJE_SALARIO = "Introduzca el porcentaje de aumento de salario: ";
+
+    /**
+     * Mensaje utilizado para mostrar la lista de empleados.
+     */
     public static final String LISTA_EMPLEADOS = "Lista de Empleados:";
 }
